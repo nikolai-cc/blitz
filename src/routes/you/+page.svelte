57100b2a43ac182b6b6ab5a0a10ac664c87ad2d4
Nikolai <nikolai@theministry.co>
2023-06-23 15:49:02 +0000

    Update +layout.svelte and +page.svelte

--- src/routes/you/+page.svelte
+++ src/routes/you/+page.svelte
@@ -1,1 +1,15 @@
+<script lang="ts">
+    import { onMount } from 'svelte'
+    let mounted = false
+    onMount(() => {setTimeout(() => mounted = true, 1556)})
+</script>
+
 are you there?
+
+<br /><br />
+
+{#if mounted}
+    it's something, alright!
+{:else}
+    ...
+{/if}
